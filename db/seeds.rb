6e1c823347ff2148f6a3008e1219eb988c9c55cf
Amanda Robinson <aackley88@gmail.com>
2018-05-22 10:33:05 -0400

    Created seed file with corrections

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do |blog|
   Blog.create!(
-    title: "My blog post #{blog}"
+    title: "My blog post #{blog}",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     )
 end
@@ -9,7 +9,7 @@ puts "10 blog posts created"
 
 5.times do |skill|
   Skill.create!(
-    title: "Rails #{skill}"
+    title: "Rails #{skill}",
     percent_utilized: 15
     )
 end
